6d9e2d4cca5387e4e13f1b5e4c5639461fdb46e5
ammar falmban <devAspirant@gmail.com>
2013-10-07 13:29:22 +0900

    Initial Commit

---
+++ LearnGestureRecognizer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnGestureRecognizer
+//
+//  Created by ammar falmban on 10/7/13.
+//  Copyright (c) 2013 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearnGestureRecognizer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LearnGestureRecognizer
+//
+//  Created by ammar falmban on 10/7/13.
+//  Copyright (c) 2013 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
